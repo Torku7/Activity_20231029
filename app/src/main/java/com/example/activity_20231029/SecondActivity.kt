9dc819e188c14f6d9a14ea727c3f7a06b7b752d6
Torku7 <saturn9908>
2023-10-29 09:55:41 +0900

    화면넘기기

---
+++ app/src/main/java/com/example/activity_20231029/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.activity_20231029
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.activity_20231029.databinding.ActivityMainBinding
+import com.example.activity_20231029.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_second)
+
+        binding.btnReturn.setOnClickListener {
+
+        }
+    }
+}
